5e83f92a123cd5eb687c644c844c12e003a21762
Diana Holland <holla@bikefarm.org>
2020-03-05 10:30:41 -0800

    Add map

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import Events from './components/Events';
 import Map from './components/Map';
 import './App.css';
 
+// FIXME: lat/long should be typed into form and pass down into components
 function App(): JSX.Element {
   return (
     <div className="root">
       <NavBar />
       <div className="content">
         <Events />
-        <Map />
+        <Map latitude={45.504738} longitude={-122.675275} />
       </div>
     </div>
   );
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { LatLngTuple } from 'leaflet';
 
-function Map(): JSX.Element {
-  return <div className="map-pane">I am a map</div>;
+interface MapProps {
+  latitude: number;
+  longitude: number;
+}
+interface MapState {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+}
+class CustomMap extends Component<MapProps, MapState> {
+  constructor(props: MapProps) {
+    const { latitude, longitude } = props;
+    super(props);
+    this.state = {
+      latitude,
+      longitude,
+      zoom: 13,
+    };
+  }
+  render(): JSX.Element {
+    const position = [this.state.latitude, this.state.longitude] as LatLngTuple;
+    return (
+      <div className="map-pane" id="map">
+        <Map center={position} zoom={this.state.zoom} className="map">
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+          ></TileLayer>
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </Map>
+      </div>
+    );
+  }
 }
 
-export default Map;
+export default CustomMap;
